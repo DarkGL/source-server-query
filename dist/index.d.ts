fab07a2631a7b0331a54bdca0f8c885a24f717bf
Rafał Więcek <gitzzz14@gmail.com>
2024-01-04 23:51:40 +0100

    feat: Add SourceQuerySocket class and its methods
    This commit adds the SourceQuerySocket class, which includes methods for querying information about game servers. The class has methods for retrieving server info, player info, and server rules. It also includes a bind method for setting up the socket connection.

---
+++ dist/index.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="node" />
+import { BindOptions } from 'dgram';
+export declare class SourceQuerySocket {
+    port?: number;
+    address?: string;
+    exclusive?: boolean;
+    fd?: number;
+    private socket?;
+    constructor(options?: BindOptions);
+    private bind;
+    private assert;
+    private close;
+    private validate;
+    private send;
+    private pack;
+    private solicit;
+    info: (address: string, port: number | string, timeout?: number) => Promise<Record<string, any>>;
+    players: (address: string, port: number | string, timeout?: number) => Promise<Record<string, string | number>[]>;
+    rules: (address: string, port: string | number, timeout?: number) => Promise<Record<string, string>[]>;
+}
+declare const _default: SourceQuerySocket;
+export default _default;
